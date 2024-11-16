5ba9dab8f2e05745224919a77b5d3bb5747d37ec
Tanay Kumar <tanaykmrdev@gmail.com>
2024-11-16 10:29:00 +0530

    feat: added signup endpoint + testing husky

--- apps/http/src/routes/v1/index.ts
+++ apps/http/src/routes/v1/index.ts
@@ -2,13 +2,36 @@ import { Router } from "express";
 import { userRouter } from "./user";
 import { spaceRouter } from "./space";
 import { adminRouter } from "./admin";
+import { SignupSchema } from "../../types";
+import client from "@repo/db/client";
 
 export const router = Router();
 
-router.get("/signup", (req, res) => {
-    res.json({
-        message: "Signup",
-    });
+router.get("/signup", async (req, res) => {
+    const parsedData = SignupSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.status(400).json({
+            message: "Message validation failed",
+            errors: parsedData.error.errors,
+        });
+        return;
+    }
+
+    try {
+        const user = await client.user.create({
+            data: {
+                username: parsedData.data.username,
+                password: parsedData.data.password,
+                role: parsedData.data.type === "admin" ? "Admin" : "User",
+            },
+        });
+
+            res.json({userId: user.id})
+
+    } catch (e) {
+        console.log("unable to create user: ", e)
+        res.status(400).json({ message: "User already exists" });
+    }
 });
 
 router.get("/signin", (req, res) => {
